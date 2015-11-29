d38f2394a83a65ac22759d7728635b27543c641d
Gyusun Yeom <omniavinco@gmail.com>
2015-11-29 17:10:12 +0900

    Add test for ResponseTypeEnum

--- command/types_test.go
+++ command/types_test.go
@@ -47,3 +47,46 @@ func TestColor2Json(t *testing.T) {
 		}
 	}
 }
+
+func TestResponseTypeEnum2Str(t *testing.T) {
+	cases := []struct {
+		enum ResponseTypeEnum
+		str  string
+	}{
+		{in_channel, "in_channel"},
+		{deffered_in_channel, "in_channel"},
+		{ephemeral, "ephemeral"},
+		{123, ""},
+	}
+
+	for _, c := range cases {
+		got := fmt.Sprintf("%s", c.enum)
+		if got != c.str {
+			t.Errorf("%d - expected %q, got %q", c.enum, c.str, got)
+		}
+	}
+}
+
+func TestResponseTypeEnum2Json(t *testing.T) {
+	cases := []struct {
+		enum ResponseTypeEnum
+		str  string
+		e    bool
+	}{
+		{in_channel, `"in_channel"`, false},
+		{deffered_in_channel, `"in_channel"`, false},
+		{ephemeral, `"ephemeral"`, false},
+		{123, "", true},
+	}
+
+	for _, c := range cases {
+		buf, e := json.Marshal(c.enum)
+		if (e != nil) != c.e {
+			t.Errorf("Error not occured. expected error")
+		}
+		got := string(buf)
+		if got != c.str {
+			t.Errorf("%d - expected %q, got %q", c.enum, c.str, got)
+		}
+	}
+}
