1e5d2cb67781137c1682520d9541728b49f83a84
Gyusun Yeom <omniavinco@gmail.com>
2015-12-05 12:24:15 +0900

    Move mock...

---
+++ util/redis_wrap_mock.go
@@ -0,0 +1,85 @@
+package util
+
+import (
+	"errors"
+	"fmt"
+	"gopkg.in/redis.v3"
+	"strconv"
+	"time"
+)
+
+type StringCmdMock struct {
+	*redis.StringCmd
+	value []byte
+	e     error
+}
+
+type RedisClientMock struct {
+	data map[string][]byte
+}
+
+func (cmd *StringCmdMock) Val() string {
+	return string(cmd.value)
+}
+
+func (cmd *StringCmdMock) Result() (string, error) {
+	return cmd.Val(), cmd.e
+}
+
+func (cmd *StringCmdMock) Bytes() ([]byte, error) {
+	return cmd.value, cmd.e
+}
+
+func (cmd *StringCmdMock) Int64() (int64, error) {
+	if cmd.e != nil {
+		return 0, cmd.e
+	}
+	return strconv.ParseInt(cmd.Val(), 10, 64)
+}
+
+func (cmd *StringCmdMock) Uint64() (uint64, error) {
+	if cmd.e != nil {
+		return 0, cmd.e
+	}
+	return strconv.ParseUint(cmd.Val(), 10, 64)
+}
+
+func (cmd *StringCmdMock) Float64() (float64, error) {
+	if cmd.e != nil {
+		return 0, cmd.e
+	}
+	return strconv.ParseFloat(cmd.Val(), 64)
+}
+
+func (cmd *StringCmdMock) Scan(val interface{}) error {
+	return nil
+}
+
+func (cmd *StringCmdMock) String() string {
+	return fmt.Sprint(cmd, cmd.value)
+}
+
+func (c *RedisClientMock) Set(key string, value interface{}, expiration time.Duration) StatusCmd {
+	ret := new(redis.StatusCmd)
+	switch value.(type) {
+	case string:
+		c.data[key] = []byte(value.(string))
+	default:
+		c.data[key] = []byte(fmt.Sprintf("%q", value))
+	}
+	return ret
+}
+
+func (c *RedisClientMock) Get(key string) StringCmd {
+	ret := &StringCmdMock{new(redis.StringCmd), []byte{}, nil}
+	if data, ok := c.data[key]; ok {
+		ret.value = data
+	} else {
+		ret.e = errors.New("Value not found")
+	}
+	return ret
+}
+
+func NewRedisMock() RedisClient {
+	return &RedisClientMock{map[string][]byte{}}
+}
