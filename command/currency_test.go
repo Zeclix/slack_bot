595386d1c1686776d379eb2f22243018fd22a6af
Gyusun Yeom <omniavinco@gmail.com>
2015-12-28 00:33:27 +0900

    Fix RE - before matches only 1 digits...
    when target currency is not present, assume it KRW

--- command/currency_test.go
+++ command/currency_test.go
@@ -3,8 +3,9 @@ package command
 import "testing"
 
 func TestCurrencyCommand(t *testing.T) {
-	ret := CurrencyCommand(Request{Text: "1USD=?KRW"})
+	ret := CurrencyCommand(Request{Text: "10 USD"})
 	if ret.ResponseType != deffered_in_channel {
 		t.Error("Response error")
 	}
+	t.Log(ret)
 }
